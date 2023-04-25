18362e6ceb79517203a2c5e7c61f7b90ce3ab39b
jeck-025 <jerrickanatalio@gmail.com>
2023-04-25 15:00:37 +0800

    Update config.php

--- resource/php/class/config.php
+++ resource/php/class/config.php
@@ -16,15 +16,15 @@ class config{
 
             //LOCAL
 
-            //$this->pdo = new PDO('mysql:host=127.0.0.1:3306;dbname=sms', $this->user, $this->password);
-           // } catch (PDOException $e) {
-                //die($e->getMessage());
+            $this->pdo = new PDO('mysql:host=127.0.0.1:3306;dbname=sms', $this->user, $this->password);
+           } catch (PDOException $e) {
+                die($e->getMessage());
 
             //HOSTINGER
-            $this->pdo = new PDO('mysql:local=109.106.254.186:3306;dbname=cemlre_sms', $this->user, $this->password);
-            } catch (PDOException $e) {
-                die($e->getMessage());
-        }
+        //     $this->pdo = new PDO('mysql:local=109.106.254.186:3306;dbname=cemlre_sms', $this->user, $this->password);
+        //     } catch (PDOException $e) {
+        //         die($e->getMessage());
+        // }
         return $this->pdo;
 
     }
